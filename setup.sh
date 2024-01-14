995a4e6c356238ed5f758e1a9d5978e95d2a85aa
pawinwat <pawinwat_wat@hotmail.com>
2024-01-15 01:01:52 +0700

    chenge mv to cp

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip3 install -r requirements.txt
 sudo chmod +x impyla-shell
-sudo mv impyla-shell /usr/local/bin/
-sudo mv impyla-config.ini /usr/local/bin/
+sudo cp impyla-shell /usr/local/bin/
+sudo cp impyla-config.ini /usr/local/bin/
 sudo chmod +x /usr/local/bin/impyla-shell
