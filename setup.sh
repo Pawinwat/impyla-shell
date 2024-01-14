e8db726f261831316c9d3a8e4934370ffac483e6
pawinwat <pawinwat_wat@hotmail.com>
2024-01-15 01:31:05 +0700

    add cp -n to config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip3 install -r requirements.txt
 sudo chmod +x impyla-shell
 sudo cp impyla-shell /usr/local/bin/
-sudo cp impyla-config.ini /usr/local/bin/
+sudo cp -n impyla-config.ini /usr/local/bin/
 sudo chmod +x /usr/local/bin/impyla-shell
